ba4ad756de58f887743038ef1bbf576c564d9a39
kovaaalex <kovalenko.alex04@gmail.com>
2024-09-14 02:34:46 +0300

    add theme

--- theme.js
+++ theme.js
@@ -2,4 +2,5 @@ const themeSwitch = document.getElementById('theme')
 
 themeSwitch.addEventListener('click', () => {
     themeSwitch.classList.toggle('night')
+    document.body.classList.toggle('night__theme')
 })
